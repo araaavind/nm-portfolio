a1169e9df769923211a4afa1f940ce67555d589c
aravindmathradan <aravindmathradan2@gmail.com>
2022-09-29 14:23:11 +0530

    server changes

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
-app.use(express.static('./dist/'));
+
+const login = require('./routes/login');
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
 app.use(express.json({ limit: '1mb' }));
+app.use('/', express.static('./dist/'));
+app.use('/login', login);
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resource not found</h1>')
+});
