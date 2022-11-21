ce449023cee3f0cb521476fda52c08d014cd3740
aravindmathradan <aravindmathradan2@gmail.com>
2022-11-21 13:34:52 +0530

    minor fixes for error 404 page

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       input: [
         resolve(__dirname, 'src/index.html'),
         resolve(__dirname, 'src/birthday/index.html'),
+        resolve(__dirname, 'src/404/index.html'),
         resolve(__dirname, 'src/admin/index.html'),
         resolve(__dirname, 'src/admin/login.html'),
         resolve(__dirname, 'src/admin/signup.html'),
